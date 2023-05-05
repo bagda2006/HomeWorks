d6f4a14ed0170f839e7cdfa99b99b03218ddbb15
bagdasar <bagdasar.aydinyan2006@gmail.com>
2023-05-05 20:51:46 +0300

    HM6 Done

---
+++ src/Basic_Level/Exam30/Test.java
@@ -0,0 +1,11 @@
+package Basic_Level.Exam30;
+
+public class Test {
+    public static void main(String[] args) {
+        String x = "xyz";
+        x.toUpperCase();
+        String y = x.replace('Y','y');
+        y = y + "abc";
+        System.out.println(y);
+    }
+}
